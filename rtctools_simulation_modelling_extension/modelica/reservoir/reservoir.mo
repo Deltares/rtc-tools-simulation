2dc9f252958acec71b6b4ef734e930ab1692067b
Sjoerd Geevers <Sjoerd.Geevers@deltares.nl>
2024-02-29 17:14:10 +0100

    reservoir: add a basic reservoir model
    Add a class for building a reservoir model.
    The user can inherit this class and apply schemes for setting inputs.
    This class contains many utiltities for applying schemes
    and uses a default Modelica model.

---
+++ rtctools_simulation_modelling_extension/modelica/reservoir/reservoir.mo
@@ -0,0 +1,27 @@
+model Reservoir
+  import SI = Modelica.SIunits;
+
+  parameter SI.Length H_crest();
+
+  input SI.VolumeFlowRate Q_in();
+  input SI.VolumeFlowRate Q_turbine();
+  input Boolean do_spill;
+
+  output SI.Volume V();
+  output SI.VolumeFlowRate Q_out();
+  output SI.Length H();
+  SI.Area Area();
+  SI.VolumeFlowRate Q_spill_from_lookup_table();
+  output SI.VolumeFlowRate Q_spill();
+
+equation
+  // Lookup tables:
+  // V -> Area
+  // V -> H
+  // H -> QSpill_from_lookup_table
+
+  der(V) = Q_in - Q_out;
+  Q_spill = do_spill * Q_spill_from_lookup_table;
+  Q_out = Q_turbine + Q_spill;
+    
+end Reservoir;
---
+++ rtctools_simulation_modelling_extension/reservoir/model.py
@@ -0,0 +1,72 @@
+"""Module for a reservoir model."""
+from datetime import datetime
+from pathlib import Path
+
+from rtctools_simulation_modelling_extension.model import Model, ModelConfig
+
+MODEL_DIR = Path(__file__).parent.parent / "modelica" / "reservoir"
+
+
+class ReservoirModel(Model):
+    """Class for a reservoir model."""
+
+    def __init__(self, config: ModelConfig, use_default_model=True, **kwargs):
+        if use_default_model:
+            config.set_dir("model", MODEL_DIR)
+            config.set_model("Reservoir")
+        super().__init__(config, **kwargs)
+
+    # Helper functions for getting the time/date.
+    def sec_to_datetime(self, time_in_seconds) -> datetime:
+        """Convert time in seconds to datetime."""
+        return self.io.sec_to_datetime(time_in_seconds, self.io.reference_datetime)
+
+    def get_next_time(self) -> float:
+        """Get the next time value (in seconds)."""
+        return self.get_current_time() + self.get_time_step()
+
+    def get_next_datetime(self) -> datetime:
+        """Get the next time value (in datetime)."""
+        return self.sec_to_datetime(self.get_next_time())
+
+    # Schemes
+    def set_q(self, value):
+        """Set Q_turbine."""
+        # TODO: this should be updated.
+        self.set_var("Q_turbine", value)
+
+    def apply_spillway(self, do_spill=True):
+        """Enable water to spill from the reservoir."""
+        self.set_var("do_spill", do_spill)
+
+    # Methods for applying schemes / setting input.
+    def set_default_input(self):
+        """Set default input values."""
+        time = self.get_current_time()
+        q_turbine = self.timeseries_at("Q_turbine", time)
+        self.set_var("Q_turbine", q_turbine)
+        self.set_var("do_spill", False)
+
+    def apply_schemes(self):
+        """
+        Apply schemes.
+
+        This method should be overwritten by the user.
+        """
+        pass
+
+    def initialize_input_variables(self):
+        """Initialize input variables."""
+        self.set_default_input()
+
+    def set_input_variables(self):
+        """Set input variables."""
+        self.set_default_input()
+        self.apply_schemes()
+
+    # Plotting
+    def get_output_variables(self):
+        variables = super().get_output_variables().copy()
+        variables.extend(["Q_in"])
+        variables.extend(["Q_turbine"])
+        return variables
