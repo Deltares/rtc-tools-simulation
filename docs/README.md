0dd2c5ec76435727a39d3bdbfe61b5398d36565c
Sjoerd Geevers <Sjoerd.Geevers@deltares.nl>
2024-03-08 16:31:47 +0100

    docs: setup documentation
    Setup the documentation.
    Most of the files are genereated by sphinx-quickstart.

---
+++ docs/README.md
@@ -0,0 +1,9 @@
+# Documentation
+
+This file contains some guidelines on writing/building documentation.
+
+## Building HTML Documentation
+
+Go to the `docs` folder and run `make html`.
+The start html page is `docs\build\html\index.html`.
+Open the path to this file in your browser to view the documentation.
---
+++ docs/source/conf.py
@@ -0,0 +1,26 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "rtc-tools-simulation-modelling-extension"
+copyright = "2024, Deltares"
+author = "Deltares"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
