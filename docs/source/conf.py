d2310a619a4146c331cd24650b0def391c98f60a
Sjoerd Geevers <Sjoerd.Geevers@deltares.nl>
2024-03-28 15:14:40 +0100

    docs: update configuration
    Update the configuration for generating the documentation.
    - Add sphinx extensions.
    - Unset the html static path.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ author = "Deltares"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["sphinx.ext.autodoc"]
 
 templates_path = ["_templates"]
 exclude_patterns = []
@@ -23,4 +23,4 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
