f4f24dc4986217ef037c8be4a7f5e28154edaa05
Sjoerd Geevers <Sjoerd.Geevers@deltares.nl>
2024-04-11 09:35:08 +0200

    tests: make sure that cli command can be found
    Make sure that the CLI command rtc-tools-reservoir-template
    can be found.
    Normally, this command can be found, but in certain debug modes
    the path environment needs to be updated.

--- tests/cli_test.py
+++ tests/cli_test.py
@@ -1,12 +1,19 @@
 """Tests for the command line interface."""
+import os
 import shutil
 import subprocess
+import sys
 from pathlib import Path
 
 
 def test_reservoir_template():
     """Test building a reservoir model template from the command line."""
     script = shutil.which("rtc-tools-reservoir-template")
+    if script is None:
+        # Hacky way to ensure that rtc-tools-reservoir-template can be found.
+        # This normally seems to be the case except for some specific debug modes.
+        os.environ["PATH"] += os.pathsep + str(Path(sys.executable).parent)
+        script = shutil.which("rtc-tools-reservoir-template")
     assert script is not None, "Unable to find the rtc-tools-reservoir-template executable."
     test_run = subprocess.run(
         [
