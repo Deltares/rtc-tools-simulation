15c8184e63015d9bb9408fb81745da09a7c67ac7
Ailbhe <ailbhe.mitchell@deltares.nl>
2024-05-31 10:25:55 +0200

    spillay example added

---
+++ examples/spillway_example/spillway_example.py
@@ -0,0 +1,37 @@
+"""Example that illustrates how to create a basic model."""
+
+from pathlib import Path
+
+from rtctools_simulation.reservoir.model import ModelConfig, ReservoirModel
+
+CONFIG = ModelConfig(base_dir=Path(__file__).parent)
+
+
+class SingleReservoir(ReservoirModel):
+    """Example single reservoir model."""
+
+    def apply_schemes(self):
+        """Apply schemes for controlling the reservoir."""
+
+        # Apply schemes.
+        h = self.get_var("H")
+        h_crest = self.get_var("H_crest")
+        if h > h_crest:
+            self.apply_spillway()
+            self.set_q(
+                target_variable="Q_turbine",
+                input_type="parameter",
+                input_data=0.6,
+                )
+        else:
+            self.set_q(
+                target_variable="Q_turbine",
+                input_type="parameter",
+                input_data=0.4,
+            )
+
+
+# Create and run the model.
+if __name__ == "__main__":
+    model = SingleReservoir(CONFIG)
+    model.simulate()
